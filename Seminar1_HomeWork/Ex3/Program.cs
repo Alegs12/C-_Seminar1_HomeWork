0be0161c99ea5df819dc8e813e0454f16b983fe6
Alexandr <algran83@mail.ru>
2022-10-28 00:52:12 +0300

    add Ex3

---
+++ Seminar1_HomeWork/Ex3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Enter number a :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1%2 == 0)
+    {
+    Console.WriteLine($"{num1} -> да");   
+    }
+else Console.WriteLine($"{num1} -> нет");
